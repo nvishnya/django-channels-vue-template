58a63c0c3f5ec0165f29d625053760a743e95276
Nastya Liashchevska <nnvishnya@gmail.com>
2019-07-17 16:54:12 +0300

    Remove lazy loading route

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import RoomInput from './components/RoomInput.vue'
+import ChatRoom from './components/ChatRoom.vue'
 
 Vue.use(Router)
 
@@ -16,7 +17,7 @@ export default new Router({
     {
       path: '/:room',
       name: 'chat',
-      component: () => import('./components/ChatRoom.vue')
+      component: ChatRoom
     }
   ]
 })
